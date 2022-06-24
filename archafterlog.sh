95c7d5f139f5db4bd11df90d69f9bc9af64ada0c
Vishwa <91792810+vishwaravi321@users.noreply.github.com>
2022-06-24 14:34:20 +0530

    Update archafterlog.sh

--- archafterlog.sh
+++ archafterlog.sh
@@ -9,9 +9,26 @@ echo "
 127.0.0.1	localhost
 ::1		localhost
 127.0.1.1	klaus" >> /etc/hosts
+#adding groot user
+useradd - m groot
+#installing grub
+pacman -Sy grub
 
-useradd groot
-
+grub-install /dev/sda
+#configuring grub
+grub-mkconfig -o /boot/grub/grub.cfg
+#install sudo
+pacman -Sy sudo
+#permissions for groot
+usermod -aG wheel,audio,video,storage groot
+#installing networkmanager
+pacman -S networkmanager 
+#enabling networkmanager
+systemctl enable NetworkManager.service
+#mentioning other tasks
+echo "Please create passwd for user groot and dont forget to configure visudo !!!!!!!!!!!!!!!"
+#create password for groot
 passwd groot
+
 
 
