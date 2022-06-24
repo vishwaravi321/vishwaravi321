5aa93f668c8ccea6ea77dc88b5b37c28b181108c
Vishwa <91792810+vishwaravi321@users.noreply.github.com>
2022-06-24 14:22:03 +0530

    Update archafterlog.sh

--- archafterlog.sh
+++ archafterlog.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #setting time
-timedatectl set-timezone Asia/Kolkatta
+timedatectl set-timezone Asia/Kolkata
 #configuring hostname
 echo "klaus" > /etc/hostname
 #host file
 echo "
 127.0.0.1	localhost
 ::1		localhost
-127.0.1.1	klaus"
+127.0.1.1	klaus" >> /etc/hosts
 
 
 echo "Please create passwd"
